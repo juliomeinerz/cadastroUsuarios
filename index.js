e217715ecf78b2e8f351d0afdb24d3844322661c
Gabriel Maia <GabrielHenriqueMaia@users.noreply.github.com>
2016-09-20 15:13:58 -0300

    Remoção da modularização
    Como este é o arquivo principal do aplicativo é desnecessária a utilização do module.exports

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var config = require('./local_modules/config.js');
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
-var router = express.Router();
 var database = require('./local_modules/database.js');
 
 var basicRoutes = require('./routes/basic');
@@ -15,7 +14,6 @@ app.set('view engine', 'pug');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
-
 //logger
 app.use(function (req, res, next) {
 	console.log(req.method + ' ' + req.url);
@@ -25,8 +23,4 @@ app.use(function (req, res, next) {
 //Roteamento
 app.use('/', basicRoutes);
 
-
-
-
 app.listen(config.port); 
-module.exports = router;
